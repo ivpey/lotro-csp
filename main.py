927a2c43eaf75f3393214feab45e8281205a69a2
ivpey <peychev.ivelin@gmail.com>
2025-07-23 19:29:47 +0200

    added session handler function; improved tooltip; saving character class info along with gear config

--- main.py
+++ main.py
@@ -36,6 +36,21 @@ stat_sheet = Stat_Table()
 # each user session gets its own class instances which we store here against a unique userID
 cipu = {}
 
+def session_handler(session):
+    if 'user' not in session:
+        session['user'] = str(uuid4().hex)
+        cipu[session['user']] = {
+            'curr_stats' : Character_Sheet(),
+            'old_stats' : Character_Sheet()
+        }
+    elif (cipu == {}): # to handle gracefully the case when e.g. the service is restarted while a user is working
+        cipu[session['user']] = {
+            'curr_stats' : Character_Sheet(),
+            'old_stats' : Character_Sheet()
+        }
+
+    return cipu[session['user']]
+
 @app.template_filter()
 def format_url_unquote(val, isItemName = False):
     formatted = urllib.parse.unquote_plus(val)
@@ -60,6 +75,15 @@ def item_tooltip(itemName):
         # fixing the image paths
         to_return = resp[tooltip_HTML_start_at : tooltip_HTML_end_at]
         to_return = to_return.replace('/images/', 'https://lotro-wiki.com/images/')
+
+        # making the item name a link
+        tooltip_item_name_start = to_return.index('<span class="qc', 0)
+        tooltip_item_name_end = to_return.index('</span>', tooltip_item_name_start) + len('</span>')
+        item_name_HTML = to_return[tooltip_item_name_start : tooltip_item_name_end]
+        to_return = to_return.replace(
+            item_name_HTML,
+            '<a href="https://lotro-wiki.com/wiki/Item:' + urllib.parse.quote_plus(itemName).replace('+', '_') + '" style="color: inherit;">' + item_name_HTML + '</a>'
+        )
     except:
         to_return = 'Tooltip could not be loaded.'
     
@@ -70,112 +94,84 @@ def encode_gear_config(gear_config):
     return b64encode(zlib.compress(json.dumps(gear_config).encode('utf-8'))).decode('utf-8')
 
 @app.route("/")
-def index():
-
-    if 'user' not in session:
-        session['user'] = str(uuid4().hex)
-        
-    cipu[session['user']] = {
-        'curr_stats' : Character_Sheet(),
-        'old_stats' : Character_Sheet()
-    }
-
-    return render_template('index.html')
-
-@app.route("/stats_panel")
 def stats_panel_page():
+    s = session_handler(session)
 
-    # if a user randomly opens this page without going through index, they do not have initialized classes
-    # => redirect so we can initialize them
-    su = session['user']
-    try:
-        t = cipu[su]['curr_stats']
-    except:
-        return redirect(url_for('index'))
-    
     return render_template(
         'stat-panel.html',
-        char_sheet = cipu[su]['curr_stats'],
+        char_sheet = s['curr_stats'],
         stat_sheet = stat_sheet,
-        old_stats = cipu[su]['old_stats'])
+        old_stats = s['old_stats'])
 
 @app.route("/handle_choices", methods = ['POST'])
 def handle_choices():
-
-    # if a user randomly opens this page without going through index, they do not have initialized classes
-    # => redirect so we can initialize them
-    su = session['user']
-    try:
-        t = cipu[su]['curr_stats']
-    except:
-        return redirect(url_for('index'))
+    s = session_handler(session)
 
     import_str = request.form.get('load_stored')
     selected_class = request.form.get('class-selection')
     
-    if (selected_class in cipu[su]['curr_stats'].class_list):
-        cipu[su]['curr_stats'].class_name = selected_class
-        return redirect(url_for('stats_panel_page'))
+    if (selected_class in s['curr_stats'].class_list):
+        s['curr_stats'].class_name = selected_class
+        return redirect(url_for('stats_panel_page', hc = 'y'))
 
     if import_str is None:
-        cipu[su]['curr_stats'].itemSlots = Character_Sheet().itemSlots
+        s['curr_stats'].itemSlots = Character_Sheet().itemSlots
     else:
         try:
-            # coming from import field
             a = json.loads(zlib.decompress(b64decode(import_str)).decode('utf-8'))
         except:
-            # loaded from local storage
-            a = ast.literal_eval(ast.literal_eval(import_str))
+            s['curr_stats'].itemSlots = Character_Sheet().itemSlots
+            flash('Import failed.')
+            return redirect(url_for('stats_panel_page', hc = 'y'))
+
         if (isinstance(a, dict)):
             e = False
             for k in a.keys():
-                if(k not in Character_Sheet().itemSlots.keys()):
-                    print('contains unknown key')
+                if(k not in Character_Sheet().itemSlots.keys() and k != 'class'):
                     e = True
+
             if not e:
-                print('we got a correct dict')
-                cipu[su]['curr_stats'].itemSlots = a
+                if ('class' in a.keys() and a['class'] in s['curr_stats'].class_list): # there is a 'class' key and contains expected value
+                    s['curr_stats'].class_name = a['class']
+                    a.pop('class')
+                    s['curr_stats'].itemSlots = a
+                elif ('class' in a.keys()): # there is a 'class' key but contains unexpected value
+                    a.pop('class')
+                    s['curr_stats'].itemSlots = a
+                else:
+                    s['curr_stats'].itemSlots = a
             else:
-                print('we got wrongly defined dict')
                 flash('Import failed.')
         else:
-            print('not instance of dict')
             flash('Import failed.')
 
-    return redirect(url_for('stats_panel_page'))
+    return redirect(url_for('stats_panel_page', hc = 'y'))
 
 @app.route("/load_items", methods = ['POST'])
 def load_items():
-
-    # if a user randomly opens this page without going through index, they do not have initialized classes
-    # => redirect so we can initialize them
-    su = session['user']
-    try:
-        t = cipu[su]['curr_stats']
-    except:
-        return redirect(url_for('index'))
+    s = session_handler(session)
 
     selected_class = request.form.get('class-selection')
-    if (selected_class in cipu[su]['curr_stats'].class_list):
-        cipu[su]['curr_stats'].class_name = selected_class
+    if (selected_class in s['curr_stats'].class_list):
+        s['curr_stats'].class_name = selected_class
 
     item_slot = request.args.get('item')
     item_name = str.strip(request.form.get(item_slot), '[ ]')
 
-    cipu[su]['old_stats'] = copy.deepcopy(cipu[su]['curr_stats'])
+    s['old_stats'] = copy.deepcopy(s['curr_stats'])
 
     # deleting an item
     if (item_slot and item_name == ''):
-        flash(cipu[su]['curr_stats'].validateSlotUpdate(item_slot, {}, None), item_slot)
+        flash(s['curr_stats'].validateSlotUpdate(item_slot, {}, None), item_slot)
     else:
         try:
-            existingItemName = cipu[su]['curr_stats'].itemSlots[item_slot]['item-info']['name']
+            existingItemName = s['curr_stats'].itemSlots[item_slot]['item-info']['name']
             newItemName = urllib.parse.quote_plus(item_name).replace('+', '_')
             
             # same item -> we make no request, only handle essences if applicable
             if (newItemName == existingItemName):
                 try:
-                    essence_count = len(cipu[su]['curr_stats'].itemSlots[item_slot]['item-info']['essences'])
+                    essence_count = len(s['curr_stats'].itemSlots[item_slot]['item-info']['essences'])
                     essences = {}
                     for i in range(essence_count):
                         essence_stat_name = request.form.get('essence_'+str(i+1)+'_stat')
@@ -188,14 +184,14 @@ def load_items():
                             }
                         else:
                             essences[str(i+1)] = {}
-                    flash(cipu[su]['curr_stats'].validateSlotUpdate(item_slot, None, essences), item_slot)
+                    flash(s['curr_stats'].validateSlotUpdate(item_slot, None, essences), item_slot)
                 except:
                     pass
             # submitted an item with a different name -> request new
             else:
                 wikiResp = QueryWiki().get(item_name)
                 if (wikiResp['item']['error'] is None):
-                    flash(cipu[su]['curr_stats'].validateSlotUpdate(item_slot, wikiResp, None), item_slot)
+                    flash(s['curr_stats'].validateSlotUpdate(item_slot, wikiResp, None), item_slot)
                 else:
                     flash(wikiResp['item']['error'], item_slot)
         
@@ -203,15 +199,15 @@ def load_items():
         except:
             wikiResp = QueryWiki().get(item_name)
             if (wikiResp['item']['error'] is None):
-                flash(cipu[su]['curr_stats'].validateSlotUpdate(item_slot, wikiResp, None), item_slot)
+                flash(s['curr_stats'].validateSlotUpdate(item_slot, wikiResp, None), item_slot)
             else:
                 flash(wikiResp['item']['error'], item_slot)
 
     return render_template(
         'stat-panel.html',
-        char_sheet = cipu[su]['curr_stats'],
+        char_sheet = s['curr_stats'],
         stat_sheet = stat_sheet,
-        old_stats = cipu[su]['old_stats'])
+        old_stats = s['old_stats'])
 
 if __name__ == '__main__':  
    app.run(host = '0.0.0.0', port = 5000)
