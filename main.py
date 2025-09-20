0ff876c82157560b07bf2b646b1d3ad2326cc70c
ivpey <peychev.ivelin@gmail.com>
2025-09-20 17:35:33 +0300

    added comments and Clear button, improved comparison logic

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 # ====================
 # import packages
 # ====================
-from flask import Flask, render_template, request, flash, redirect, url_for, session, make_response, send_from_directory
-import requests
+import jinja2
+from flask import Flask, render_template, request, flash, redirect, url_for, session, make_response, send_from_directory, Blueprint
 import requests_cache
+from datetime import timedelta
 import urllib
 import copy
-import ast
-from uuid import uuid4
-from base64 import b64encode, b64decode
+from base64 import b64decode
 import zlib
-from datetime import timedelta
 import json
+from uuid import uuid4
+
+# ====================
+# import the custom filters
+# ====================
+import filters
 
 # ====================
 # import the custom class definitions
 # ====================
 from class_Character_Sheet import Character_Sheet
-from class_StatTable import Stat_Table
 from class_QueryWiki import QueryWiki
+from class_StatTable import Stat_Table
 
 # ====================
 # instantiate the app
@@ -26,6 +30,7 @@ from class_QueryWiki import QueryWiki
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'AMRSONPL'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours = 24)
+app.register_blueprint(filters.f_bp)
 
 requests_cache.install_cache('lotro-csp-cache', expire_after=86400, allowable_methods=('GET'))
 
@@ -59,48 +64,7 @@ def lcsp_cache_static(filename):
     resp.headers['Cache-Control'] = 'max-age'
     return resp
 
-@app.template_filter()
-def format_url_unquote(val, isItemName = False):
-    formatted = urllib.parse.unquote_plus(val)
-    if (isItemName == True):
-        formatted = formatted.replace('_', ' ')
-    return formatted
-
-@app.template_filter()
-def item_tooltip(itemName):
-    try:
-        resp = requests.get(
-            url = 'https://lotro-wiki.com/wiki',
-            params = {
-                'action' : 'render',
-                'title' : 'Item:' + itemName
-            }
-        ).text
-
-        tooltip_HTML_start_at = resp.index('<div class="tooltip-content itemtooltip"', 0)
-        tooltip_HTML_end_at = resp.index('</ul></div>', tooltip_HTML_start_at) + len('</ul></div>')
-
-        # fixing the image paths
-        to_return = resp[tooltip_HTML_start_at : tooltip_HTML_end_at]
-        to_return = to_return.replace('/images/', 'https://lotro-wiki.com/images/')
-
-        # making the item name a link
-        tooltip_item_name_start = to_return.index('<span class="qc', 0)
-        tooltip_item_name_end = to_return.index('</span>', tooltip_item_name_start) + len('</span>')
-        item_name_HTML = to_return[tooltip_item_name_start : tooltip_item_name_end]
-        to_return = to_return.replace(
-            item_name_HTML,
-            '<a href="https://lotro-wiki.com/wiki/Item:' + urllib.parse.quote_plus(itemName).replace('+', '_') + '" style="color: inherit;">' + item_name_HTML + '</a>'
-        )
-    except:
-        to_return = 'Tooltip could not be loaded.'
-    
-    return to_return
-
-@app.template_filter()
-def encode_gear_config(gear_config):
-    return b64encode(zlib.compress(json.dumps(gear_config).encode('utf-8'))).decode('utf-8')
-
+# main
 @app.route("/")
 def stats_panel_page():
     s = session_handler(session)
@@ -111,57 +75,70 @@ def stats_panel_page():
         stat_sheet = stat_sheet,
         old_stats = s['old_stats'])
 
+# this route handles both loading from LocalStorage, and from manual import;
+# it is intentional to treat them the same way and to pass both through the same form with the input field load_stored;
+# if we wanted to differentiate them, the JS at the beginning of the HTML page could do another formData.append(...) for a src,
+# while the Import form could have a pre-populated hidden input for src
+#
+# the route also handles class choice selection
 @app.route("/handle_choices", methods = ['POST'])
 def handle_choices():
     s = session_handler(session)
 
+    # making sure that when we import, the active config from before the import is used for the comparison,
+    # this is practically a clumsy way of enabling comparison between two custom configs
+    #
+    # the cases when we clear all items or load from LocalStorage would result in deltas which were 100% up or down,
+    # this is handled in the HTML;
+    # also in the HTML we handle deltas on class change - simply do not show them, like the other two cases
+    s['old_stats'] = copy.deepcopy(s['curr_stats'])
+
     import_str = request.form.get('load_stored')
     selected_class = request.form.get('class-selection')
+
+    # the request is made from the Clear button
+    if import_str is None and selected_class is None:
+        s['curr_stats'].itemSlots = Character_Sheet().itemSlots
+        return redirect(url_for('stats_panel_page', hc = 'y'))
     
+    # only handling class choice selection
     if (selected_class in s['curr_stats'].class_list):
         s['curr_stats'].class_name = selected_class
         return redirect(url_for('stats_panel_page', hc = 'y'))
-
-    if import_str is None:
+    
+    # try to parse the imported string
+    try:
+        a = json.loads(zlib.decompress(b64decode(import_str)).decode('utf-8'))
+    except:
         s['curr_stats'].itemSlots = Character_Sheet().itemSlots
-    else:
-        try:
-            a = json.loads(zlib.decompress(b64decode(import_str)).decode('utf-8'))
-        except:
-            s['curr_stats'].itemSlots = Character_Sheet().itemSlots
+        flash('Import failed.')
+        return redirect(url_for('stats_panel_page', hc = 'y'))
+
+    # making sure we received the expected structure and values
+    if (not isinstance(a, dict)):
+        flash('Import failed.')
+        return redirect(url_for('stats_panel_page', hc = 'y'))
+    
+    for k in a.keys():
+        if (k not in Character_Sheet().itemSlots.keys() and k != 'class'):
             flash('Import failed.')
             return redirect(url_for('stats_panel_page', hc = 'y'))
 
-        if (isinstance(a, dict)):
-            e = False
-            for k in a.keys():
-                if(k not in Character_Sheet().itemSlots.keys() and k != 'class'):
-                    e = True
-
-            if not e:
-                if ('class' in a.keys() and a['class'] in s['curr_stats'].class_list): # there is a 'class' key and contains expected value
-                    s['curr_stats'].class_name = a['class']
-                    a.pop('class')
-                    s['curr_stats'].itemSlots = a
-                elif ('class' in a.keys()): # there is a 'class' key but contains unexpected value
-                    a.pop('class')
-                    s['curr_stats'].itemSlots = a
-                else:
-                    s['curr_stats'].itemSlots = a
-            else:
-                flash('Import failed.')
-        else:
-            flash('Import failed.')
+    if ('class' in a.keys() and a['class'] in s['curr_stats'].class_list): # there is a 'class' key and contains expected value
+        s['curr_stats'].class_name = a['class']
+        a.pop('class')
+        s['curr_stats'].itemSlots = a
+    elif ('class' in a.keys()): # there is a 'class' key but contains unexpected value
+        a.pop('class')
+        s['curr_stats'].itemSlots = a
+    else:
+        s['curr_stats'].itemSlots = a
 
     return redirect(url_for('stats_panel_page', hc = 'y'))
 
 @app.route("/load_items", methods = ['POST'])
 def load_items():
     s = session_handler(session)
-
-    selected_class = request.form.get('class-selection')
-    if (selected_class in s['curr_stats'].class_list):
-        s['curr_stats'].class_name = selected_class
 
     item_slot = request.args.get('item')
     item_name = str.strip(request.form.get(item_slot), '[ ]')
